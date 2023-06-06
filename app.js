fe9ebbe05ced928a3177c1b52208063e05fdea91
JONATHA FAGUNDES GARCIA <771200801@fspoa.br>
2023-06-06 20:18:20 -0300

    Adicionando Routers e Controller

--- app.js
+++ app.js
@@ -1,71 +1,15 @@
 const express = require('express');
-const cadastroProdutos = require('./cadastro_produtos');
+const produtoRota = require('.rotas/produto_rotas')
+
 const app = express();
 const port = 3000;
 
-const produtos = [{id:1, nome: "Produto 1", preco: 30}];
+// const produtos = [{id:1, nome: "Produto 1", preco: 30}];
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World.')
-})
+app.use('/produtos', produtoRota)
 
 app.listen(port, () => {
     console.log(`Servidor Iniciado na porta ${port}`);
-})
-
-app.get('/produtos', (req, res) => {
-    // res.send("Listando produtos: ");
-    const listaProdutos = cadastroProdutos.listar();
-    res.json(listaProdutos);
-})
-
-app.get('/produtos/:id', (req, res) => {
-    //res.send("Buscar pelo id: ");
-    const id = req.params.id;
-    try {
-        const produto = cadastroProdutos.buscarPorId(id);
-        res.json(produtos);
-    } catch (err) {
-        res.status(err.numero).json(err);
-    }
-})
-
-app.post('/produtos', (req, res) => {
-    //res.send("Cadastrar produto: ");
-    const produto = req.body;
-
-    try{
-        const produtoInserido = cadastroProdutos.inserir();
-        res.status(201).json(produtoInserido);
-    }
-    catch (err) {
-        res.status(err.numero).json(err);
-    }
-})
-
-app.put('/produtos/:id', (req, res) => {
-    //res.send("Atualizar o produto com id: ");
-    const id = req.params.id;
-    const produto = req.body;
-    try {
-        const produtoAtualizado = cadastroProdutos.atualizar(id,produto);
-        res.json(produtoAtualizado);
-    }
-    catch (err) {
-        res.status(err.numero).json(err);
-    }
-})
-
-app.delete('/produtos/:id', (req, res) => {
-    //res.send("Deletando o produto com id: ");
-    const id = req.params.id;
-    try {
-        const produtoDeletado = cadastroProdutos.deletar(id);
-        res.json(produtoDeletado);
-    }
-    catch (err) {
-        res.status(err.numero).json(err);
-    }
 })
